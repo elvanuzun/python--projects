946394d7c04681605be2136732eddfc719ce0938
Elvan Uzun <elvanuzun98@gmail.com>
2022-07-24 21:18:40 +0300

    Add question list

---
+++ 9-Quiz-Game/question_list.py
@@ -0,0 +1,34 @@
+from classes import Questions as Q
+
+questions_list = [
+   Q(
+        "Sokrates'in Savunması, Devlet ve Şölen kitaplarının yazarı kimdir?", 
+        ["A- Platon", "B-Sokrates", "C-Aristoteles"], 
+        "A"
+        ),
+    
+    Q(
+        "Nobel edebiyat ödülünü reddeden, Sinekler, Duvar ve Bulantı kitaplarıyla bilinen varoluşçu filozofun adı nedir?", 
+        ["A-Nietzche", "B-Kierkegaard", "C-Camus"], 
+        "C"
+        ),
+    
+    Q(
+        "Nihilizm kelimesi ilk defa hangi kitapla dünyada duyuldu?", 
+        ["A-Sefiller",  "B-Babalar ve Oğullar","C-Felsefenin Tesellisi"], 
+        "B"), 
+    
+    Q(
+        "Felsefe tarihinin ilk düşünce suçlusu sayılan baldıran zehiri içirilerek idam edilen ünlü düşünür kimdir?", 
+        ["A-Sokrates","B-Descartes", "C-Thales"], 
+        "A"
+        ), 
+    
+    Q(
+        "Dünya Felsefe Günü Kasım ayının kaçıncı perşembesi kutlanır?", 
+        ["A-bir", "B-iki","C-üç"], 
+        "C"
+        ),
+    
+
+]
